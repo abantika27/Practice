8180a203ce4e4aa098adddfdc9594e5f3d4f8314
abantika27 <96277040+abantika27@users.noreply.github.com>
2024-09-06 18:05:25 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,9 @@
+# print("Hello, World!")
+# i am learning python
+
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+print("False or False is ", False or False)
+print("False or False is ", False or False)
