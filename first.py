cb7b8d093064365c4aa74a43e866a7890b522e5e
Abantika Dasgupta <abantikadg.work@gmail.com>
2024-09-09 17:42:11 +0530

    added content

--- first.py
+++ first.py
@@ -1,9 +1,27 @@
 # print("Hello, World!")
 # i am learning python
 
+#comparison operators
+d = 5==5
+print(d)
+
+#logical operators
 print("True or False is ", True or False)
 print("True or True is ", True or True)
 print("False or True is ", False or True)
 print("False or False is ", False or False)
 print("False or False is ", False or False)
 print("False or False is ", False or False)
+
+a = "31.2"
+b = float(a) 
+t = type(b) # type of b
+
+#by default input is string
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+print("The square of the number is", a**2) #a^2 would be incorrect
+print("Number a is: ", a)
+print("Number b is: ", b) 
+print("Sum is ", a + b)
+
